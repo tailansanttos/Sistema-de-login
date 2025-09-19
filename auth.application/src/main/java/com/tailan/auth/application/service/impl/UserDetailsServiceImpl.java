9124ef47efb467846d6f4b7aeee8c81844c2af2e
tailan <tailansanttos02@gmail.com>
2025-09-19 10:17:42 -0300

    feat: service de userdetails para carregar detalhes do usuario com o username(no caso o email) passado.

---
+++ auth.application/src/main/java/com/tailan/auth/application/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.tailan.auth.application.service.impl;
+
+import com.tailan.auth.application.handle.exceptions.UserNotFoundException;
+import com.tailan.auth.application.model.domain.User;
+import com.tailan.auth.application.model.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UserNotFoundException("User with this " + username + " not found!"));
+        return user;
+    }
+}
