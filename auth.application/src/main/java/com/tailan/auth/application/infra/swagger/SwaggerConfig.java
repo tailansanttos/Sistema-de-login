0fba20df6ab2cdf80dc2163b35fe8e4cdf08b7f5
tailan <tailansanttos02@gmail.com>
2025-09-19 11:12:34 -0300

    refactor: config do swagger

--- auth.application/src/main/java/com/tailan/auth/application/infra/swagger/SwaggerConfig.java
+++ auth.application/src/main/java/com/tailan/auth/application/infra/swagger/SwaggerConfig.java
@@ -1,22 +1,16 @@
 package com.tailan.auth.application.infra.swagger;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("Login API").description("Login API").version("v1.0"));
     }
+
 }
