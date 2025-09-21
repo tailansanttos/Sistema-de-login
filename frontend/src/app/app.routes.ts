44f47da5d1f409a7cb07a70e20fe05bde620f3dc
tailan <tailansanttos02@gmail.com>
2025-09-21 00:01:10 -0300

    deleted

--- frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { Login } from './pages/login/login';
-
-export const routes: Routes = [
-    {
-        path: 'login',
-        component: Login
-    }
-];
